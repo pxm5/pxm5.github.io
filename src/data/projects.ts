32845bd6333a2772fe531f4c588921c53a36bc36
Preetham <preethu.mamill@gmail.com>
2026-03-30 22:46:40 -0700

    Fixed incorrect image reference path.

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -12,7 +12,7 @@ export const projects: Project[] = [
     title: "Research Paper Implementations",
     description:
       "A collection of implementations of foundational ML models that I implemented from their respective research papers.",
-    image: "https://github.com/pxm5/pxm5.github.io/blob/main/src/images/alexnet_img.png",
+    image: "https://raw.githubusercontent.com/pxm5/pxm5.github.io/refs/heads/main/src/images/alexnet_img.png",
     tags: ["Python", "PyTorch", "Computer Vision"],
     link: "",
   },
